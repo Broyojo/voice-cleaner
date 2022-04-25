31ef9c6093bea1aa5422cb3111f580eabbe0d4ca
Broyojo <dha@xoba.com>
2022-04-25 15:57:26 -0400

    improved loading dataset and saving code

--- dataset.py
+++ dataset.py
@@ -5,8 +5,33 @@ import struct
 
 
 def load_dataset(path):
-    pass
+    tracks = []
+    with alive_bar(len(os.listdir(path)), dual_line=True, title="Loading Dataset...") as bar:
+        for file in os.listdir(path):
+            if file.endswith(".raw"):
+                name = os.path.join(path, file)
+                bar.text = f"Loading {name}..."
+                data = Path(name).read_bytes()
+                samples = []
+                for i in range(0, len(data), 2):
+                    # read two bytes at a time
+                    sample = struct.unpack("h", data[i:i+2])
+                    # normalize audio between 0 and 1
+                    samples.append(sample[0] / 32767)
+                if len(samples) == 22050:  # pad with zeros in the future
+                    tracks.append(samples)
+            bar()
+        bar.title = "Loading Dataset... Done!"
+    return tracks
 
 
-def write_file(path, data):
-    pass
+def save_output(path, data):
+    with alive_bar(len(data), dual_line=True, title="Saving Output...") as bar:
+        for s, sample in enumerate(data):
+            name = os.path.join(path, f"{s}.raw")
+            with open(name, "wb") as f:
+                for i in range(len(sample)):
+                    data = struct.pack("h", round(sample[i]))
+                    f.write(data)
+            bar()
+        bar.title = "Saving Output... Done!"
--- train.py
+++ train.py
@@ -1,33 +1,14 @@
 from autoencoder import AutoEncoder
-import os
-import struct
-from alive_progress import alive_bar
-from pathlib import Path
+from dataset import load_dataset
 
 
 def main():
     ae = AutoEncoder(input_dim=22050, latent_dim=2205,
                      save_path="models/first_run.h5")
 
-    X = []
+    X = load_dataset("data/train/")
 
-    l = len(os.listdir("data/train/"))
-    with alive_bar(l) as bar:
-        for file in os.listdir("data/train/"):
-            if file.endswith(".raw"):
-                name = os.path.join("data/train/", file)
-
-                data = Path(name).read_bytes()
-
-                samples = []
-
-                for i in range(0, len(data), 2):
-                    sample = struct.unpack("h", data[i:i+2])
-                    samples.append(sample[0] / 32767)
-
-                if len(samples) == 22050:  # pad with zeros in the future
-                    X.append(samples)
-            bar()
+    return
 
     ae.train(X, X)
 
